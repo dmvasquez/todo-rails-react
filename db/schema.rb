55d08b23c83094dae46a2ebdafb569693990f2fb
David Vasquez <davidvasquez@Davids-MBP-2.localdomain>
2019-06-10 09:18:08 -0700

    updates to database in ps

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_04_223129) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "done"
